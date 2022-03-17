cc5b2df9ba3e43642e1ea7c40d839df9c71ceab9
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-17 12:36:37 +0600

    first commit

---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+
+const Countries = () => {
+    const [countries, setCounties] = useState([]);
+
+    useEffect(() =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCounties(data));
+    },[])
+
+
+    return (
+        <div>
+            <h1>Hello from Countries:{countries.length}</h1>
+            {
+                countries.map(country => console.log(country))
+            }
+            {
+                countries.map(country => <Country 
+                    country = {country}
+                    // name={country.name.common} 
+                    // population={country.population}
+                    // area={country.area}
+                    // region={country.region}
+                    
+                    ></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/Components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Country.css"
+
+const Country = (props) => {
+    console.log(props);
+    const {area,region,population,name} = props.country;
+    return (
+        <div className='country'>
+            <h2>Country Name: {name.common}</h2>
+            <p>Population: {population}</p>
+            <p><small> Region :{region}</small></p>
+            <p><small>Area: {area}</small></p>
+        </div>
+    );
+};
+
+export default Country;
