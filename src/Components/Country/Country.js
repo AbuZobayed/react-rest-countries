1b551937e1d592b30b45348600283ee5ddb7103b
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-17 12:48:30 +0600

    46-6

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -2,14 +2,13 @@ import React from 'react';
 import "./Country.css"
 
 const Country = (props) => {
-    console.log(props);
-    const {area,region,population,name} = props.country;
+    const {area, region,name} = props.country
+   
     return (
         <div className='country'>
-            <h2>Country Name: {name.common}</h2>
-            <p>Population: {population}</p>
-            <p><small> Region :{region}</small></p>
-            <p><small>Area: {area}</small></p>
+            <h2>Name: {name.common}</h2>
+            <p>Area: {area}</p>
+            <p><small> Region: {region}</small></p>
         </div>
     );
 };
